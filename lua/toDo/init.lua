08d0460a8daed79a2bf7d7eb7f5ac0ed639c2d94
facu-glo <gabylope05@gmail.com>
2025-04-26 16:32:13 -0300

    primer commit

---
+++ lua/toDo/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.setup = function(opts)
+    opts = opts or {}
+    local path = opts.path or "~/toDo.md"
+
+    vim.keymap.set("n", "<leader>td", function()
+        vim.cmd("edit " .. path)
+        local bufnr = vim.fn.bufnr(path)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<CMD>bdelete<CR>",
+            { noremap = true, silent = true })
+    end, { desc = "Abrir ToDo.md" })
+end
+
+
+
+return M
